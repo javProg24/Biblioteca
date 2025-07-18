b79494a8184da4215a886fc59ad8e4e35abcedd6
javProg24 <javiertomalag14@gmail.com>
2025-07-18 00:55:49 -0500

    Definiendo desde afuera que columna especifico

--- src/main/resources/Shared/Table/TableActionCellEditor.java
+++ src/main/resources/Shared/Table/TableActionCellEditor.java
@@ -7,11 +7,9 @@ import java.awt.*;
 public class TableActionCellEditor extends AbstractCellEditor implements TableCellEditor {
     private final TableActionEvent event;
     private int estadoColumnIndex;
-    private String condicion;
-    public TableActionCellEditor(TableActionEvent event, int estadoColumnIndex,String condicion) {
+    public TableActionCellEditor(TableActionEvent event, int estadoColumnIndex) {
         this.event = event;
         this.estadoColumnIndex = estadoColumnIndex;
-        this.condicion=condicion;
     }
     public TableActionCellEditor(TableActionEvent event) {
         this.event = event;
@@ -22,11 +20,11 @@ public class TableActionCellEditor extends AbstractCellEditor implements TableCe
         PanelAction panel = new PanelAction();
 
         Object estadoValue = table.getValueAt(row, estadoColumnIndex);
-        boolean habilitado = !(condicion.equals(estadoValue));
+        boolean habilitado = !("No disponible".equals(estadoValue));
 
         panel.setEnabled(habilitado);
         panel.initEvent(habilitado ? event : null, table::getEditingRow);
-        panel.setToolTipText(habilitado ? null : "Recurso no disponible");
+        panel.setToolTipText(habilitado ? null : "Ejemplar no disponible");
 
         return panel;
     }
